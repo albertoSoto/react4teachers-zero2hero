5cd1f7ea77cdcae505628e1f0769153d3be9909b
Alberto Soto <alberto.soto@gmail.com>
2022-02-16 07:29:25 +0400

    chapter 3 theory

---
+++ ch_3_react_usecases/src/App.js
@@ -0,0 +1,13 @@
+
+import {Hero} from "./components/Hero"
+import Header from "./layout/Header"
+function App() {
+  return (
+      <body className="text-gray-700 bg-white">
+        <Header/>
+      <Hero/>
+      </body>
+  );
+}
+
+export default App;
