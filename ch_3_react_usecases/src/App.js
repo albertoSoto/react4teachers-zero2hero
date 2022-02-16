72ca29ab9896ed6688161e903b7040672be93201
Alberto Soto <alberto.soto@gmail.com>
2022-02-16 21:32:04 +0400

    chapter 3 exercise part 1

--- ch_3_react_usecases/src/App.js
+++ ch_3_react_usecases/src/App.js
@@ -8,14 +8,28 @@ class App extends React.Component
 {
     constructor(props) {
         super(props);
+        this.state = {
+            option: 1
+        }
     }
-
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({option: 2})
+        }, 10000)
+    }
     render() {
+        let renderingPart ="";
+        console.log(`Current state value is ${this.state.option}`)
+        if (this.state.option===2){
+            renderingPart = <Exercise2Image/>
+        }else if(this.state.option===3){
+            renderingPart = <Exercise3Data/>
+        }else {
+            renderingPart = <Hero/>
+        }
         return  <body className="text-gray-700 bg-white">
             <Header/>
-            <Hero/>
-            <Exercise2Image/>
-            <Exercise3Data/>
+            {renderingPart}
         </body>
     }
 }
