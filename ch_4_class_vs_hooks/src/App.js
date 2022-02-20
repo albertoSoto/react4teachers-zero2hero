90ff1f716577665edf316efcf59b6461889d7739
Alberto Soto <alberto.soto@gmail.com>
2022-02-20 09:20:33 +0400

    chapter 4 component solution

--- ch_4_class_vs_hooks/src/App.js
+++ ch_4_class_vs_hooks/src/App.js
@@ -1,39 +1,34 @@
 import {Hero} from "./components/Hero"
 import Header from "./layout/Header"
-import React from 'react'
+import React, { useState } from 'react';
 import Exercise2Image from "./components/Exercise2Image"
 import Exercise3Data from "./components/Exercise3Data";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            option: 1
-        }
-        this.setActiveOption = this.setActiveOption.bind(this);
-    }
-
-
-    setActiveOption(val) {
+const App = () =>{
+    const [option, setOption] = useState(1);
+    const setActiveOption = (val) => {
         console.log(`Callback - changing state value to ${val}`)
-        this.setState({option: val})
+        setOption(val);
     }
-
-    render() {
-        let renderingPart = "";
-        console.log(`Current state value is ${this.state.option}`)
-        if (this.state.option === 2) {
-            renderingPart = <Exercise2Image/>
-        } else if (this.state.option === 3) {
-            renderingPart = <Exercise3Data/>
-        } else {
+    let renderingPart = "";
+    switch (option){
+        case 1:
             renderingPart = <Hero/>
-        }
-        return <body className="text-gray-700 bg-white">
-        <Header onClick={this.setActiveOption}/>
-        {renderingPart}
-        </body>
+            break;
+        case 2:
+            renderingPart = <Exercise2Image/>
+            break;
+        case 3:
+            renderingPart = <Exercise3Data/>
+            break;
+        default:
+            break;
     }
+    console.log(`Current state value is ${option}`)
+    return (<body className="text-gray-700 bg-white">
+    <Header onClick={setActiveOption}/>
+    {renderingPart}
+    </body>)
 }
 
 export default App;
