9d379cc45a275c795c2af99693481805785de034
Alberto Soto <alberto.soto@gmail.com>
2022-02-20 08:28:13 +0400

    chapter 4 boilerplate

---
+++ ch_4_class_vs_hooks/src/App.js
@@ -0,0 +1,44 @@
+import {Hero} from "./components/Hero"
+import Header from "./layout/Header"
+import React from 'react'
+import Exercise2Image from "./components/Exercise2Image"
+import Exercise3Data from "./components/Exercise3Data";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            option: 1
+        }
+        this.setActiveOption = this.setActiveOption.bind(this);
+    }
+
+    componentDidMount() {
+        /*setTimeout(() => {
+            this.setActiveOption(2)
+        }, 10000)*/
+    }
+
+    setActiveOption(val) {
+        console.log(`Callback - changing state value to ${val}`)
+        this.setState({option: val})
+    }
+
+    render() {
+        let renderingPart = "";
+        console.log(`Current state value is ${this.state.option}`)
+        if (this.state.option === 2) {
+            renderingPart = <Exercise2Image/>
+        } else if (this.state.option === 3) {
+            renderingPart = <Exercise3Data/>
+        } else {
+            renderingPart = <Hero/>
+        }
+        return <body className="text-gray-700 bg-white">
+        <Header onClick={this.setActiveOption}/>
+        {renderingPart}
+        </body>
+    }
+}
+
+export default App;
---
+++ ch_4_class_vs_hooks/src/components/Exercise2Image.js
@@ -0,0 +1,37 @@
+
+import React from 'react'
+
+class Exercise2Image extends React.Component
+{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+            <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://picsum.photos/384/512"
+                 alt="" width="384" height="512"/>
+                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                    <blockquote>
+                        <p className="text-lg font-medium">
+                            “Tailwind CSS is the only framework that I've seen scale
+                            on large teams. It’s easy to customize, adapts to any design,
+                            and the build size is tiny.”
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium">
+                        <div className="text-sky-500 dark:text-sky-400">
+                            Sarah Dayan
+                        </div>
+                        <div className="text-slate-700 dark:text-slate-500">
+                            Staff Engineer, Algolia
+                        </div>
+                    </figcaption>
+                </div>
+        </figure>
+        )
+    }
+}
+
+export default Exercise2Image;
